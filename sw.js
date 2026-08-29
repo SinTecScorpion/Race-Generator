2e7cf511623d913181f42076d6befd7ff59b8cfa
SinTecScorpion <98954213+SinTecScorpion@users.noreply.github.com>
2026-08-29 16:45:43 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,5 @@
-const CACHE='race-generator-pwa-v3';
-const CORE=[
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './Icons/icon-192.png',
-  './Icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() =>
-      caches.match(event.request).then(hit => hit || caches.match('./index.html'))
-    )
-  );
-});
+const CACHE='race-generator-manual-fusion-v4';
+const CORE=['./','./index.html','./manifest.webmanifest','./Icons/icon-192.png','./Icons/icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
