2740345ee3b54a1bb3dfacb5ddb07a91767510d2
SinTecScorpion <98954213+SinTecScorpion@users.noreply.github.com>
2026-08-29 21:13:43 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='race-generator-step09-quality-network-first-v1';
+const CACHE='race-generator-step10-master-network-first-v1';
 const CORE=[
   './manifest.webmanifest',
   './Icons/icon-192.png',
