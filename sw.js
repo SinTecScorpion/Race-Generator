22889c744df83124f399a7ceb03a80534b1b310d
SinTecScorpion <98954213+SinTecScorpion@users.noreply.github.com>
2026-08-29 23:06:13 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'race-generator-r3-anatomy-allowlist-forms-v1';
+const CACHE = 'race-generator-r4-saved-library-filters-v1';
 const OFFLINE_HTML='./index.html';
 const CORE=[
   './manifest.webmanifest',
